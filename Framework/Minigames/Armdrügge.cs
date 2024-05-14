49b49e658f35d6742318698f9cf0cc95fba8f64f
Amos Forrer <amos.forrer@gmail.com>
2024-05-14 23:32:50 +0200

    view upgrade

--- Framework/Minigames/Armdrügge.cs
+++ Framework/Minigames/Armdrügge.cs
@@ -19,7 +19,13 @@ public class MyMinigame6 : MinigameDefBase
     [Element]
     public Rectangle Rectright { get; set;}
     [Element]
-    public Rectangle Rectfill { get; set;}
+    public Rectangle Rectrightr { get; set;}
+    [Element]
+    public Rectangle Rectleftr { get; set;}
+    [Element]
+    public Rectangle Recttopr { get; set;}
+    [Element]
+    public Rectangle Rectbottomr { get; set;}
 
     public GameObjectContainer<Rectangle> RectRot {get; set;} = new();
 
@@ -29,22 +35,35 @@ public class MyMinigame6 : MinigameDefBase
     {
         Rectleft = new()
         {
-            Id = "leftrectb",
             X = 150,
             Y = 150,
             Width = 20,
             Height = 700,
             Fill = "black",
             // OnClick = spawnRect,
-            
+        };
 
-        
+        Rectleftr = new()
+        {
+            X = 1350,
+            Y = 150,
+            Width = 20,
+            Height = 700,
+            Fill = "black",
         };
    
         Recttop = new()
         {
-            Id = "toprectb",
             X = 150,
+            Y = 150,
+            Width = 100,
+            Height = 20,
+            Fill = "black",
+        };
+
+        Recttopr = new()
+        {
+            X = 1350,
             Y = 150,
             Width = 100,
             Height = 20,
@@ -53,8 +72,16 @@ public class MyMinigame6 : MinigameDefBase
 
         Rectright = new()
         {
-            Id = "rightrectb",
             X = 249,
+            Y = 150,
+            Width = 20,
+            Height = 700,
+            Fill = "black",
+        };
+
+        Rectrightr = new()
+        {
+            X = 1449,
             Y = 150,
             Width = 20,
             Height = 700,
@@ -63,18 +90,43 @@ public class MyMinigame6 : MinigameDefBase
 
         Rectbottom= new()
         {
-            Id = "bottomrectb",
             X = 150,
             Y = 830,
             Width = 100,
             Height = 20,
             Fill = "black",
         };
+
+        Rectbottomr= new()
+        {
+            X = 1350,
+            Y = 830,
+            Width = 100,
+            Height = 20,
+            Fill = "black",
+        };
+
+        Rects.Add(Rectbottomr);
+        Rects.Add(Recttopr);
+        Rects.Add(Rectleftr);
+        Rects.Add(Rectrightr);
         Rects.Add(Rectbottom);
         Rects.Add(Rectright);
         Rects.Add(Recttop);
         Rects.Add(Rectleft);
 
+        AddElement(
+        new Rectangle()
+        {
+            X = 200,
+            Y = 730,
+            Width = 19,
+            Height = 60,
+            Fill = "transparent",
+            Stroke = "orange",
+            StrokeWidth = 100
+        }
+        );
   
         Update();
     }
